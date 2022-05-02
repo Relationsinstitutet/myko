79178f7505452c2f2a6e5977f6a95de4718599ef
Samuel Gulliksson <samuel@relationsinstitutet.org>
2022-05-02 16:19:31 +0200

    Add Auth0 SDK and initial integration.

---
+++ myko/src/routes/login/callback.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { goto } from '$app/navigation';
+  import createClient from '$lib/auth/client';
+
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    const client = await createClient();
+    const appStateFromBeforeLogin = await client.handleLoginCallback();
+
+    let returnTo = '/';
+    if (appStateFromBeforeLogin) {
+      // successful login, update login state
+      // TODO log something?
+      console.log('hello!');
+      console.log(appStateFromBeforeLogin);
+      returnTo = appStateFromBeforeLogin.returnTo ?? returnTo;
+    }
+
+    // TODO log unsolicited request to redirect URI?
+
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    return goto(returnTo, { replaceState: true });
+  });
+</script>
+
+Loggar in...
