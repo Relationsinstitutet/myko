97b2385b307b985ea7dd4ae18c10255d6b12f0a7
Samuel Gulliksson <samuel@relationsinstitutet.org>
2022-05-02 17:21:19 +0200

    Add scaffolding for P5.js.

--- myko/src/routes/index.svelte
+++ myko/src/routes/index.svelte
@@ -1,18 +1,49 @@
 <script>
-  // populated with data from the endpoint
-  export let activities;
+  import { onMount } from 'svelte';
+
+  function p5Sketch() {
+    const sketch = (p) => {
+      let x = 0;
+      let y = 0;
+      let size = 15;
+      let threshold = 0;
+
+      p.setup = function() {
+        const c = p.createCanvas(700, 410);
+        c.parent('canvasContainer');
+      };
+
+      p.draw = function() {
+        // TODO replace with 'art' visualization
+        p.stroke(1);
+        threshold = p.random(0.75);
+
+        if (threshold < 0.1) p.line(x, y, x + size, y + size);
+        else if (0.505 > threshold > 0.5) p.line(x, y, x, y + size);
+        else p.line(x, y + size, x + size, y);
+
+        x = x + size;
+        if (x > p.width) {
+          x = 0;
+          y = y + size;
+        }
+      };
+    };
+
+    new p5(sketch);
+  }
+
+  onMount(p5Sketch);
 </script>
 
 <svelte:head>
-  <title>Aktiviteter</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.1/p5.min.js"
+          integrity="sha512-NxocnqsXP3zm0Xb42zqVMvjQIktKEpTIbCXXyhBPxqGZHqhcOXHs4pXI/GoZ8lE+2NJONRifuBpi9DxC58L0Lw=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer">
+  </script>
 </svelte:head>
 
-<h1>Aktivititer</h1>
+<div id="canvasContainer"></div>
 
-<ul>
-  {#each activities as activity}
-    <li>
-      <a href="/activities/{activity.slug.current}">{activity.name}</a>
-    </li>
-  {/each}
-</ul>
+<a href="/activities">Aktiviteter</a>
