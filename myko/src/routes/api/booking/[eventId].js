b7deb9c9197c49db33873e20a73387b24947ab57
mipodi <43996895+mipodi@users.noreply.github.com>
2022-05-09 11:29:34 +0200

    Booking flow step 1 (#6)
    * Adds working dotenv configuration to prepare for robot tokens
    
    * Disables all Auth0 to work easier with booking button
    
    * Adds non-working but maybe closer to working patch than no patch at all
    
    * Adds user to event as attendee, hardcoded the only user for now

--- myko/src/routes/api/booking/[eventId].js
+++ myko/src/routes/api/booking/[eventId].js
@@ -1,21 +1,35 @@
+import sanityClient from '@sanity/client';
+import { createWriteClient } from '$lib/sanityClient';
+import _ from '$lib/env';
+
 // Register booking for user authenticated via Bearer token
 export async function post({ params: { eventId }, request }) {
-  const auth = request.headers.get('Authorization');
-
-  // TODO can the userid be extracted directly from the access token?
-  const userinfoResponse = await fetch('https://relationsinstitutet.eu.auth0.com/userinfo', {
-    headers: {
-      Authorization: auth,
-    },
-  });
-  const userinfo = await userinfoResponse.json();
-  console.log(userinfo.sub);
+  // const auth = request.headers.get('Authorization');
+  //
+  // // TODO can the userid be extracted directly from the access token?
+  // const userinfoResponse = await fetch('https://relationsinstitutet.eu.auth0.com/userinfo', {
+  //   headers: {
+  //     Authorization: auth,
+  //   },
+  // });
+  // const userinfo = await userinfoResponse.json();
+  // console.log(userinfo.sub);
   console.log(`eventId: ${eventId}`);
 
   // TODO add user with id from `userinfo.sub` to event with id `eventId` in Sanity
 
-  const data = {};
+  const data = await createWriteClient()
+    .patch(eventId)
+    .setIfMissing({ attendees: [] })
+    .insert('after', 'attendees[-1]', [
+      { _type: 'webusers', _ref: '069ed43a-9670-4c1e-9abe-a2e0f6bd701f' },
+    ])
+    .commit({
+      autoGenerateArrayKeys: true,
+    });
+
   if (data) {
+    console.log(data);
     return {
       status: 200,
       body: data,
