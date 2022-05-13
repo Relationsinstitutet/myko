18713c150527ea1783e78a35f60454d5ad7c0807
Mipodi <juliamjohansson@gmail.com>
2022-05-13 15:32:06 +0200

    Updates the isRegistered variable see issue #17

--- myko/src/lib/components/BookingControls.svelte
+++ myko/src/lib/components/BookingControls.svelte
@@ -44,6 +44,7 @@
     // console.log('Not logged in, redirect to login');
     // authClient.login(currentPath);
     // }
+    isRegistered = true;
   }
 
   async function handleCancelClick() {
@@ -62,6 +63,7 @@
     // console.log('Not logged in, redirect to login');
     // authClient.login(currentPath);
     // }
+    isRegistered = false;
   }
 
   export let eventId: string;
