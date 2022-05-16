1d99397b83b6401e4a37fec33a4be506b4b93b1e
Samuel Gulliksson <samuel@relationsinstitutet.org>
2022-05-16 09:36:52 +0200

    Add custom error page.

---
+++ myko/src/routes/__error.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+  export function load({ error, status }) {
+    return {
+      props: {
+        status: status,
+        message: error.message,
+      },
+    };
+  }
+</script>
+
+<script lang="ts">
+  export let message: string;
+  export let status: number;
+</script>
+
+{#if status === 404}
+  Uh-oh, we couldn't find that!
+{:else}
+  Something went wrong!
+{/if}
