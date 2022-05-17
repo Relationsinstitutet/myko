ee21924e4124b949a92642ea571dd0e1be91a4c1
Mipodi <juliamjohansson@gmail.com>
2022-05-17 10:57:54 +0200

    Adds diy route with form that logs submissions to console

---
+++ myko/src/routes/activities/diy.svelte
@@ -0,0 +1,32 @@
+<script>
+  const submitForm = (data) => {
+    const formData = new FormData(data.currentTarget);
+    // console.log(formData.get('name'));
+  };
+</script>
+
+<svelte:head>
+  <title>Tillverka aktivitet</title>
+</svelte:head>
+
+<h1>Tillverka aktivitet</h1>
+
+<form on:submit|preventDefault={submitForm}>
+  <div>
+    <label for="">
+      Name
+      <input type="text" name="name" />
+    </label>
+  </div>
+
+  <div>
+    <label for="">
+      Email
+      <input type="email" name="email" />
+    </label>
+  </div>
+
+  <div>
+    <input type="submit" />
+  </div>
+</form>
