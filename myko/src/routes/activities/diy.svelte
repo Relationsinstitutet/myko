eea9ee9e7bd628b513162fe9354ab8b6027fc4f7
Mipodi <juliamjohansson@gmail.com>
2022-05-19 13:38:25 +0200

    Configures correct activity description fields with labels and cleans up code

--- myko/src/routes/activities/diy.svelte
+++ myko/src/routes/activities/diy.svelte
@@ -20,20 +20,29 @@
 <!-- <form action="/activities/diy"> -->
 <form method="post">
   <div>
+    <label for="activity-description">
+      Hur går aktiviteten till?
+      <span>
+        Beskriv kort eller långt vad du eller ni gör. Några förslag är hur lång tid det tar och om
+        det skulle kunna bli en aktivitet som alla kan göra samt kanske vad den skulle kunna ha för
+        arbetsnamn.
+      </span>
+      <input type="activity-description" name="activity-description" />
+    </label>
+  </div>
+
+  <div>
     <label for="">
-      Name
+      Vem är du/ni som gör aktiviteten?
+      <span>
+        Valfritt. Bra att veta om det är en aktivitet för flera och om vi har frågor eller vill
+        följa upp.
+      </span>
       <input type="text" name="name" />
     </label>
   </div>
 
   <div>
-    <label for="">
-      Email
-      <input type="email" name="email" />
-    </label>
-  </div>
-
-  <div>
-    <input type="submit" />
+    <input type="submit" value="Notera aktiviteten" />
   </div>
 </form>
--- myko/src/routes/activities/diy.ts
+++ myko/src/routes/activities/diy.ts
@@ -1,3 +1,29 @@
+export async function post({ request }) {
+  const data = await request.formData(); // or .json(), or .text(), etc
+
+  const name = data.get('name');
+  const activityDesc = data.get('activity-description');
+  const url = `https://docs.google.com/forms/d/e/1FAIpQLSdS9DRucqH91-yj6Ntf2G15ubI0G8kIMNwY07qyye8kZcsacA/formResponse?usp=pp_url&entry.1469164367=${activityDesc}&entry.1714084245=${name}&submit=Submit`;
+
+  const res = await fetch(url);
+
+  // console.log(await res.json());
+
+  return { status: 201 };
+}
+
+//   // const submitForm = async (data: any) => {
+//   //   const formData = new FormData(data.currentTarget);
+//   //   const res = await fetch('diy.ts', {
+//   //     method: 'POST',
+//   //     body: formData,
+//   //   });
+//   //
+//   //   // const { message } = await res.json();
+//   // };
+// };
+//
+
 // export const post = async ({ request }) => {
 //   const body = await request.formData();
 //   const name = body.get('name');
@@ -23,51 +49,3 @@
 // console.log('hej');
 // const formData = new FormData(data.currentTarget);
 // console.log(formData);
-
-/** @type {import('@sveltejs/kit').RequestHandler} */
-export async function post({ request }) {
-  const data = await request.formData(); // or .json(), or .text(), etc
-  console.log('data');
-
-  // await create(data);
-  // console.log(data.get('name'));
-  const name = data.get('name');
-  const email = data.get('email');
-  const url = `https://docs.google.com/forms/d/e/1FAIpQLSdS9DRucqH91-yj6Ntf2G15ubI0G8kIMNwY07qyye8kZcsacA/formResponse?usp=pp_url&entry.1714084245=${name}&entry.1620787639=${email}&submit=Submit`;
-
-  console.log(url);
-
-  const res = await fetch(
-    `https://docs.google.com/forms/d/e/1FAIpQLSdS9DRucqH91-yj6Ntf2G15ubI0G8kIMNwY07qyye8kZcsacA/formResponse?usp=pp_url&entry.1714084245=${name}&entry.1620787639=${email}&submit=Submit`
-  );
-
-  console.log(await res.text());
-
-  return { status: 201 };
-}
-
-// export const post: RequestHandler<{ eventId: string }, ResponseBody> = async ({
-//   params: { eventId },
-//   locals,
-// }) => {
-//   console.log(`eventId: ${eventId}`);
-//
-//   const userdata = locals.user;
-//   if (!userdata) {
-//     return {
-//       status: 401,
-//     };
-//   }
-// };
-//
-//   // const submitForm = async (data: any) => {
-//   //   const formData = new FormData(data.currentTarget);
-//   //   const res = await fetch('diy.ts', {
-//   //     method: 'POST',
-//   //     body: formData,
-//   //   });
-//   //
-//   //   // const { message } = await res.json();
-//   // };
-// };
-//
