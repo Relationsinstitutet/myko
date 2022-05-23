092aa2ad5a04c2872fa029a32547cc92613ace60
Samuel Gulliksson <samuel@relationsinstitutet.org>
2022-05-23 13:05:29 +0200

    Add /api/activities/log, returning public data from activity log.
    Currently limited to the 10 most recent entries.

---
+++ myko/src/routes/api/activities/log.ts
@@ -0,0 +1,33 @@
+import { createReadClient } from '$lib/sanityClient';
+import { sanitySchemaNames } from '$lib/util';
+import type { RequestHandler, ResponseBody } from '@sveltejs/kit';
+
+type SanityResultType = {
+  _createdAt: string;
+  activitySlug: string;
+};
+
+// Fetch activity log
+export const get: RequestHandler<Record<string, string>, ResponseBody> = async () => {
+  const limit = 10;
+  const client = await createReadClient();
+  const activityLogQuery = `*[_type == "${sanitySchemaNames.activitylog}"] |
+    order(_createdAt desc) [0..${limit - 1}]
+    {
+        _createdAt,
+        "activitySlug": activity->slug.current
+    }`;
+
+  const entries = await client.fetch<SanityResultType[]>(activityLogQuery);
+
+  const body = entries.map((e) => {
+    return {
+      date: e._createdAt,
+      activity: e.activitySlug,
+    };
+  });
+  return {
+    status: 200,
+    body: { body },
+  };
+};
