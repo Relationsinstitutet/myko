e370e0db9d68ba85b321cfde00490a5df0b5a475
Mipodi <juliamjohansson@gmail.com>
2022-05-31 17:25:07 +0200

    Adds nav bar styles and fixes to bottom, amazing first start

--- myko/src/routes/__layout.svelte
+++ myko/src/routes/__layout.svelte
@@ -1,8 +1,33 @@
 <nav>
-  <a href="/activities">göra</a>
-  <a href="/">vi</a>
-  <a href="/settings">jag</a>
-  <a href="/faq">?</a>
+  <div class="bottom-nav">
+    <a href="/activities">göra</a>
+    <a href="/">vi</a>
+    <a href="/settings">jag</a>
+    <a href="/faq">?</a>
+  </div>
 </nav>
 
 <slot />
+
+<style>
+  nav a {
+    color: red;
+  }
+
+  .bottom-nav {
+    color: #5856d6;
+    position: fixed;
+    bottom: 0;
+    overflow: hidden;
+    border-top: 0.5px solid #ccc;
+    width: 100%;
+    padding-top: 0.8rem;
+    padding-bottom: 0.8rem;
+    background-color: #fafafa;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-evenly;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
