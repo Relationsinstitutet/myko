4e3e1cd6cf76beb76825d3aa30870139171d646f
Mipodi <juliamjohansson@gmail.com>
2022-06-02 15:14:26 +0200

    Replaces hardcoded values with variables

--- myko/src/routes/__layout.svelte
+++ myko/src/routes/__layout.svelte
@@ -15,29 +15,67 @@
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&display=swap');
-  /* nav a {
-    color: red;
-  } */
+
+  nav {
+    /* Font sizes */
+    --14px: 0.875rem;
+    --16px: 1rem;
+    --18px: 1.125rem;
+    --20px: 1.25rem;
+    --24px: 1.5rem;
+    --30px: 1.875rem;
+    --36px: 2.25rem;
+    --48px: 3rem;
+    --60px: 3.75rem;
+    --72px: 4.5rem;
+
+    /* Grey colors */
+    --grey-900: hsla(0, 0%, 13%, 1); /* Typography */
+    --grey-800: hsla(0, 0%, 23%, 1); /* Typography */
+    --grey-700: hsla(0, 0%, 32%, 1);
+    --grey-600: hsla(0, 0%, 38%, 1); /* Typography */
+    --grey-500: hsla(0, 0%, 49%, 1);
+    --grey-400: hsla(0, 0%, 62%, 1);
+    --grey-300: hsla(0, 0%, 69%, 1);
+    --grey-200: hsla(0, 0%, 81%, 1);
+    --grey-100: hsla(0, 0%, 88%, 1);
+    --grey-050: hsla(0, 0%, 97%, 1);
+
+    /* Ocean colors */
+    --ocean-800: hsla(185, 97%, 23%, 1);
+    --ocean-600: hsla(185, 99%, 29%, 1);
+    --ocean-400: hsla(187, 54%, 51%, 1);
+    --ocean-100: hsla(152, 43%, 93%, 1);
+
+    /* Peach colors */
+    --peach-700: hsla(358, 93%, 79%, 1);
+    --peach-500: hsla(30, 100%, 85%, 1);
+    --peach-300: hsla(28, 100%, 90%, 1);
+    --peach-100: hsla(49, 100%, 96%, 1);
+
+    /* Purple colors */
+    --purple-700: hsla(311, 26%, 49%, 1);
+    --purple-500: hsla(314, 23%, 59%, 1);
+    --purple-400: hsla(332, 49%, 70%, 1);
+    --purple-200: hsla(341, 50%, 85%, 1);
+  }
 
   .nav-bottom {
-    background-color: hsla(49, 100%, 96%, 1);
+    background-color: var(--peach-100);
     /* border-top: 0.5px solid #ccc; */
-    border-bottom: 2px solid hsla(185, 99%, 29%, 1);
+    border-bottom: 2px solid var(--ocean-600);
     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
 
     position: fixed;
     bottom: 0;
     overflow: hidden;
     margin-bottom: 8px;
-
     height: 48px;
     width: 100%;
-    /* padding-top: 0.8rem;
-    padding-bottom: 0.8rem; */
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
-    /* justify-content: space-evenly; */
+
     justify-content: space-between;
     align-items: center;
 
@@ -47,7 +85,7 @@
 
   .nav-bottom a {
     text-decoration: none;
-    color: hsla(0, 0%, 23%, 1);
+    color: var(--grey-800);
   }
   /*
   [aria-current] .navbottom a {
