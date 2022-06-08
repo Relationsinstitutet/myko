e97f6e7b47502df9813cd020c7c1757f66f7512a
Mipodi <juliamjohansson@gmail.com>
2022-06-08 15:01:10 +0200

    Styles and refactors diy page

--- myko/src/routes/activities/[slug].svelte
+++ myko/src/routes/activities/[slug].svelte
@@ -100,7 +100,7 @@
     padding-top: 48px;
     padding-left: 48px;
     padding-right: 48px;
-    padding-bottom: 200px;
+    padding-bottom: 256px;
     font-family: 'Lato', sans-serif;
     color: var(--grey-800);
   }
--- myko/src/routes/activities/diy.svelte
+++ myko/src/routes/activities/diy.svelte
@@ -22,6 +22,7 @@
   <title>Tillverka aktivitet</title>
 </svelte:head>
 
+<main>
 <h1>Tillverka aktivitet</h1>
 
 {#if errorMessage}
@@ -34,23 +35,24 @@
   <div>
     <label for="activity-description">
       Hur går aktiviteten till?
-      <span>
-        Beskriv kort eller långt vad du eller ni gör. Några förslag är hur lång tid det tar och om
-        det skulle kunna bli en aktivitet som alla kan göra samt kanske vad den skulle kunna ha för
-        arbetsnamn.
-      </span>
-      <input type="text" name="activity-description" required />
     </label>
+    <p>
+      Beskriv kort eller långt vad du eller ni gör. Några förslag är hur lång tid det tar och om
+      det skulle kunna bli en aktivitet som alla kan göra samt kanske vad den skulle kunna ha för
+      arbetsnamn.
+    </p>
+    <input type="text" name="activity-description" required />
   </div>
 
   <div>
     <label for="">
       Vem är du/ni som gör aktiviteten?
-      <span>
-        Bra att veta om det är en aktivitet för flera och om vi har frågor eller vill följa upp.
-      </span>
-      <input type="text" name="name" />
+
     </label>
+    <p>
+      Bra att veta om det är en aktivitet för flera och om vi har frågor eller vill följa upp.
+    </p>
+    <input type="text" name="name" />
   </div>
 
   <div>
@@ -58,8 +60,43 @@
   </div>
 </form>
 
+</main>
 <style>
-  p {
-    color: red;
-  }
+
+main {
+  background-color: var(--peach-300);
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding-top: 48px;
+  padding-left: 48px;
+  padding-right: 48px;
+  padding-bottom: 256px;
+  font-family: 'Lato', sans-serif;
+  color: var(--grey-800);
+}
+
+h1 {
+  font-family: 'Roboto Mono', monospace;
+  font-weight: 400;
+  text-transform: uppercase;
+  font-size: var(--24px);
+  color: var(--grey-800);
+  margin-top: 1em;
+  margin-bottom: var(--30px);
+}
+
+
+label {
+    --grey-800: hsla(0, 0%, 23%, 1); /* Typography */
+
+    font-size: 1rem;
+      color: var(--grey-800);
+      margin-top: 2em;
+      font-weight: 700;
+      display: block;
+}
+
+
+/* TODO: add errorMessage styling */
 </style>
