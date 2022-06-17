0a95854e58f3de8f83eef8fa0d1f0be8df58020a
Mipodi <juliamjohansson@gmail.com>
2022-06-17 12:24:15 +0200

    Adds a new faq route

---
+++ myko/src/routes/faq/index.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+
+</script>
+
+<main>
+  <h1>Vad är nu detta</h1>
+
+
+</main>
+
+<style>
+main {
+  background-color: var(--ocean-100);
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding-top: 48px;
+  padding-left: 48px;
+  padding-right: 48px;
+  padding-bottom: 256px;
+  font-family: 'Lato', sans-serif;
+  color: var(--grey-800);
+}
+
+h1 {
+  font-family: 'Roboto Mono', monospace;
+  font-weight: 400;
+  text-transform: uppercase;
+  font-size: var(--24px);
+  color: var(--grey-800);
+  margin-top: 1em;
+  margin-bottom: var(--30px);
+}
+
+ul {
+  list-style: none;
+  padding-left: unset;
+}
+
+</style>
