29f342755dff73b20b0fc2929ae8aa50ddaa1435
Mipodi <juliamjohansson@gmail.com>
2022-08-04 13:16:36 +0200

    Tries to format time on samtid view unsuccessful

--- myko/src/routes/samtid/index.svelte
+++ myko/src/routes/samtid/index.svelte
@@ -1,8 +1,68 @@
 <script lang="ts">
   import CotimeInfo from '$lib/components/cotime/CotimeInfo.svelte';
-  import type { Cotime, IActivitySummary } from '$lib/models/activity';
+  import type { Cotime } from '$lib/models/activity';
 
-  export let activities: IActivitySummary[];
+  import type { Client } from '$lib/auth/client';
+  import createClient from '$lib/auth/client';
+  import { isAuthenticated, user } from '$lib/auth/store';
+  import Paginated from '$lib/components/Paginated.svelte';
+  import { formatDate, formatTime } from '$lib/dateFormat';
+
+  import { onMount } from 'svelte';
+  import { get } from 'svelte/store';
+
+  type CompletedActivity = {
+    readonly date: string;
+    readonly time: string;
+    readonly activityName: string;
+  };
+
+  let authClient: Client;
+  let eventsUserIsAttending: {
+    readonly id: string;
+    readonly date: string;
+    readonly time: string;
+    readonly activity: {
+      readonly name: string;
+      readonly slug: string;
+    };
+    userIsAttending: boolean;
+  }[] = [];
+  let completedActivities: CompletedActivity[] = [];
+
+  onMount(async () => {
+    authClient = await createClient();
+    await authClient.updateState();
+
+    const accessToken = await authClient.getUserAccessToken();
+    const response = await fetch('/api/me', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      ({ eventsUserIsAttending, completedActivities } = data);
+    }
+  });
+
+  function logout() {
+    authClient.logout();
+  }
+
+  function login() {
+    authClient.login(currentPath);
+  }
+
+  function renderCompletedActivity(activity: CompletedActivity): string {
+    const date = formatDate(activity.date, { day: 'numeric', month: 'numeric' });
+    const time = formatTime(activity.date, activity.time);
+
+    return `<li>${date} ${time}: ${activity.activityName}</li>`;
+  }
+
+
   export let nextUpcomingCotime: Cotime | undefined = undefined;
 </script>
 
@@ -13,15 +73,52 @@
   {#if nextUpcomingCotime}
     <CotimeInfo cotime={nextUpcomingCotime} />
   {/if}
-
-  <ul>
-    <!-- TODO: configure index.ts -->
-    <!-- {#each activities as activity}
+  <h1>När det är samtid</h1>
+  <!-- {#if events.length < 1}
+  Finns inga schemalagda samtider just nu. 
+  {:else} -->
+  <ul class="plain-list">
+    {#each eventsUserIsAttending as event}
       <li>
-        <a href="/activities/{activity.slug}">{activity.name}</a>
+        <a href="/activities/{event.activity.slug}">
+          {formatDate(event.date, { day: 'numeric', month: 'numeric' })}
+          {formatTime(event.date, event.time)}
+
+          {event.activity.name}
+        </a>
       </li>
-    {/each} -->
+    {/each}
   </ul>
+
+
+
+  {#if $isAuthenticated}
+  <h1>Aktiviteter du ska vara med på</h1>
+  {#if eventsUserIsAttending.length < 1}
+    Inget inbokat än.
+  {:else}
+    <ul class="plain-list">
+      {#each eventsUserIsAttending as event}
+        <li>
+          <a href="/activities/{event.activity.slug}">
+            {formatDate(event.date, { day: 'numeric', month: 'numeric' })}
+            {event.activity.name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+
+  <h1>Genomförda aktiviteter</h1>
+  {#if completedActivities.length < 1}
+    Inga genomförda aktiviteter än.
+  {/if}
+  <ul>
+    <Paginated data={completedActivities} render={renderCompletedActivity} />
+  </ul>
+{:else}
+  <div class="unauthenticated">Logga in för att se dina bokade aktiviteter</div>
+{/if}
 </main>
 
 <style>
@@ -37,9 +134,8 @@
   }
 
   ul {
-    all: unset;
     list-style: none;
-    padding-inline-start: 0;
+    padding-left: unset;
   }
 
   li {
@@ -73,5 +169,12 @@
 
   ::marker {
     all: unset;
+  }
+
+  .plain-list {
+    list-style: none;
+    padding-left: unset;
+    display: flex;
+    flex-direction: column;
   }
 </style>
