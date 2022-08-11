4a5c16655934d7359f182948d71fb3a141fa41d6
Samuel Gulliksson <samuel@relationsinstitutet.org>
2022-08-11 20:01:03 +0200

    Remove repeated code for fetching next cotime.

--- myko/src/routes/samtid/index.ts
+++ myko/src/routes/samtid/index.ts
@@ -1,29 +1,7 @@
 import type { SanityActivityType } from '$lib/models/activity';
-import { createReadClient, eventsForActivityFilter, notDraft } from '$lib/sanityClient';
+import { activityWithNearestEventQuery, createReadClient, notDraft } from '$lib/sanityClient';
 import { computeNextCotime, sanitySchemaNames } from '$lib/util';
 import type { RequestHandler, ResponseBody } from '@sveltejs/kit';
-
-function getActivityWithNearestEvent() {
-  const eventsQuery = `*[
-    ${eventsForActivityFilter}
-  ] | order(date asc) {
-      _id,
-      attendees[]->{
-        _id,
-        "displayName": nickname
-      },
-      date,
-      "numAttendees": coalesce(count(attendees), 0)
-  }`;
-
-  return `*[
-    _type == "${sanitySchemaNames.activity}" && ${notDraft}
-  ] {
-    name,
-    "events": ${eventsQuery},
-    "slug": slug.current
-  } [count(events) > 0] | order(events[0].date) [0]`;
-}
 
 function getAllEvents() {
   const eventsQuery = `*[
@@ -42,7 +20,7 @@ function getAllEvents() {
 
 export const get: RequestHandler<Record<string, string>, ResponseBody> = async () => {
   const client = await createReadClient();
-  const activity = await client.fetch<SanityActivityType>(getActivityWithNearestEvent());
+  const activity = await client.fetch<SanityActivityType>(activityWithNearestEventQuery);
   const events = await client.fetch<SanityActivityType>(getAllEvents());
 
   if (activity) {
