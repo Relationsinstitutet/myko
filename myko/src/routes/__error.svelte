e06daa32f14fc542c10b0716b453be30c21c567d
Nellie Johnsson <nellie.johnsson@yahoo.se>
2022-09-02 19:51:40 +0200

    Styling error page

--- myko/src/routes/__error.svelte
+++ myko/src/routes/__error.svelte
@@ -13,8 +13,43 @@
   export let status: number;
 </script>
 
-{#if status === 404}
-  Uh-oh, we couldn't find that!
-{:else}
-  Something went wrong!
-{/if}
+<main>
+  {#if status === 404}
+    <h1>Attans, det blev en 404</h1>
+    <div class="error-message">
+      <p>
+        Sidan kunde inte hittas, alltså. Du kan testa ladda om sidan eller klicka i menyn, och se om
+        det går bättre.
+      </p>
+      <p>Om du vill kan du meddela oss på <span> info@relationsinstitutet.gmail</span></p>
+    </div>
+  {:else}
+    <h1>Tusan, något gick fel!</h1>
+    <p class="error-message">
+      Du kan testa att ladda om sidan, eller klicka i menyn och se om det går bättre.
+    </p>
+  {/if}
+</main>
+
+<style>
+  h1 {
+    margin: 0;
+    color: var(--ocean-600);
+  }
+  .error-message {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    font-weight: 400;
+    font-size: var(--16px);
+    color: var(--ocean-900);
+    padding-top: 2rem;
+  }
+
+  span {
+    display: inline;
+    margin-left: 8px;
+    font-weight: bold;
+    text-decoration: underline;
+  }
+</style>
