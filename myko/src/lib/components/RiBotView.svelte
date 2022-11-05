6f320933827ea0af4d813bf296196156f1d88022
Samuel Gulliksson <zamzterz@users.noreply.github.com>
2022-11-05 13:44:59 +0100

    Add Ribot chatbot as custom activity 'bli-medlem'. (#131)

---
+++ myko/src/lib/components/RiBotView.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import PortableText from '@portabletext/svelte';
+  import type { PortableTextBlocks } from '@portabletext/svelte/ptTypes';
+  import { onDestroy } from 'svelte';
+
+  onDestroy(() => {
+    document.getElementById('tidio-chat-code')?.remove();
+    document.getElementById('tidio-chat')?.remove();
+  });
+
+  export let instructions: PortableTextBlocks;
+</script>
+
+<main>
+  <h1>Bli medlem</h1>
+
+  <PortableText blocks={instructions} />
+
+  <script src="//code.tidio.co/vbj0zee1lpdkildtba49hrvoe0fpeg2r.js" async></script>
+</main>
+
+<style>
+  main {
+    background-color: var(--peach-300);
+    background-image: linear-gradient(
+      var(--peach-100),
+      var(--peach-300),
+      var(--peach-500),
+      var(--ocean-100)
+    );
+    padding-bottom: 144px;
+  }
+</style>
