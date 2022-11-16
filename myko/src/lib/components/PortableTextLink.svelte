db737ef79399d8f70f7d80a3804dbcdad5e6f93b
Samuel Gulliksson <samuel@relationsinstitutet.org>
2022-11-16 22:35:06 +0100

    Fix linting.

--- myko/src/lib/components/PortableTextLink.svelte
+++ myko/src/lib/components/PortableTextLink.svelte
@@ -9,7 +9,6 @@
   // Remember to make your variables reactive so that they can reflect prop changes
   $: mark = portableText.mark;
   $: target = mark.href?.startsWith('/') ? '_self' : 'external'; // treat relative link as "internal" link, letting Svelte handle it properly
-
 </script>
 
 {#if mark.href}
